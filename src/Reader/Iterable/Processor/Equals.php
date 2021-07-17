ea7cf032a570b54577edca76499b0eb5d3a852e0
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2021-07-17 14:43:20 +0300

    Correct work iterable processors with non-exists fields

--- src/Reader/Iterable/Processor/Equals.php
+++ src/Reader/Iterable/Processor/Equals.php
@@ -6,6 +6,8 @@ namespace Yiisoft\Data\Reader\Iterable\Processor;
 
 use Yiisoft\Data\Reader\Filter\FilterProcessorInterface;
 
+use function array_key_exists;
+
 class Equals implements IterableProcessorInterface, FilterProcessorInterface
 {
     public function getOperator(): string
@@ -19,6 +21,6 @@ class Equals implements IterableProcessorInterface, FilterProcessorInterface
             throw new \InvalidArgumentException('$arguments should contain exactly two elements');
         }
         [$field, $value] = $arguments;
-        return $item[$field] == $value;
+        return array_key_exists($field, $item) && $item[$field] == $value;
     }
 }
