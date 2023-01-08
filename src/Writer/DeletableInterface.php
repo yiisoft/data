0875314d250653506da90c261a3b8557a9afbcdc
Alexander Makarov <sam@rmcreative.ru>
2023-01-08 23:54:28 +0400

    Document writer

--- src/Writer/DeletableInterface.php
+++ src/Writer/DeletableInterface.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 
 namespace Yiisoft\Data\Writer;
 
+/**
+ * Deletable allows deleting a set of items.
+ */
 interface DeletableInterface
 {
+    /**
+     * Delete items specified.
+     *
+     * @param iterable $items Items to delete.
+     * @throws DataWriterException If there is an error deleting items.
+     */
     public function delete(iterable $items): void;
 }
