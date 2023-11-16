44c011f1fa254b884c76661e8a7ab508cc394f28
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-11-16 13:03:44 +0300

    Fix comment

--- src/Reader/ReadableDataInterface.php
+++ src/Reader/ReadableDataInterface.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 namespace Yiisoft\Data\Reader;
 
 /**
- * Readable data is a data set that could be read up to number of items
- * defined by limit either one by one or by getting an iterator.
+ * Readable data is a data set that could be read up by getting an iterator or reading one item from set.
  *
  * @template TKey as array-key
  * @template TValue as array|object
