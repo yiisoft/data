b0e356ba7e85dfd6438cc2d23f983e1272ea3ce2
Alexey Rogachev <arogachev90@gmail.com>
2024-08-27 18:03:14 +0500

    Review fix

--- src/Reader/Iterable/FilterHandler/LikeHandler.php
+++ src/Reader/Iterable/FilterHandler/LikeHandler.php
@@ -32,7 +32,7 @@ final class LikeHandler implements IterableFilterHandlerInterface
 
         /** @infection-ignore-all MBString No suitable test case was found yet. */
         return $filter->isCaseSensitive() === true
-            ? mb_stripos($itemValue, $filter->getValue()) !== false
-            : mb_strpos($itemValue, $filter->getValue()) !== false;
+            ? mb_strpos($itemValue, $filter->getValue()) !== false
+            : mb_stripos($itemValue, $filter->getValue()) !== false;
     }
 }
