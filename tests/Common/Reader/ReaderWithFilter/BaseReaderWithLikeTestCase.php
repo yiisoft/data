f029839fd244acc4afd63a0abfedc5cb3fe74251
Alexey Rogachev <arogachev90@gmail.com>
2024-09-26 21:42:25 +0500

    More test cases

--- tests/Common/Reader/ReaderWithFilter/BaseReaderWithLikeTestCase.php
+++ tests/Common/Reader/ReaderWithFilter/BaseReaderWithLikeTestCase.php
@@ -13,18 +13,20 @@ abstract class BaseReaderWithLikeTestCase extends BaseReaderTestCase
     public static function dataWithReader(): array
     {
         return [
-            'case matches, starts with search string' => ['email', 'seed@', [2]],
-            'case matches, ends with search string' => ['email', '@beat', [2]],
-            'case matches, contains search string' => ['email', 'ed@be', [2]],
-            'case does not match' => ['email', 'SEED@', [2]],
-            'wildcard is not supported' => ['email', '%st', []],
+            'case matches, starts with search string, case sensitive: null' => ['email', 'seed@', null, [2]],
+            'case matches, ends with search string, case sensitive: null' => ['email', '@beat', null, [2]],
+            'case matches, contains search string, case sensitive: null' => ['email', 'ed@be', null, [2]],
+            'case matches, contains search string, case sensitive: true' => ['email', 'ed@be', true, [2]],
+            'case matches, contains search string, case sensitive: false' => ['email', 'ed@be', false, [2]],
+            'case does not match, contains search string, case sensitive: null' => ['email', 'SEED@', null, [2]],
+            'wildcard is not supported' => ['email', '%st', null, []],
         ];
     }
 
     #[DataProvider('dataWithReader')]
-    public function testWithReader(string $field, string $value, array $expectedFixtureIndexes): void
+    public function testWithReader(string $field, string $value, bool|null $caseSensitive, array $expectedFixtureIndexes): void
     {
-        $reader = $this->getReader()->withFilter(new Like($field, $value));
+        $reader = $this->getReader()->withFilter(new Like($field, $value, $caseSensitive));
         $this->assertFixtures($expectedFixtureIndexes, $reader->read());
     }
 }
