798cc77275e1595805742c8aece7deb73e7d0559
Alexey Rogachev <arogachev90@gmail.com>
2024-10-03 13:44:53 +0500

    Test cases - fix naming, add more

--- tests/Common/Reader/ReaderWithFilter/BaseReaderWithLikeTestCase.php
+++ tests/Common/Reader/ReaderWithFilter/BaseReaderWithLikeTestCase.php
@@ -13,18 +13,25 @@ abstract class BaseReaderWithLikeTestCase extends BaseReaderTestCase
     public static function dataWithReader(): array
     {
         return [
-            'case matches, starts with search string, case sensitive: null' => ['email', 'seed@', null, [2]],
-            'case matches, ends with search string, case sensitive: null' => ['email', '@beat', null, [2]],
-            'case matches, contains search string, case sensitive: null' => ['email', 'ed@be', null, [2]],
-            'case matches, contains search string, case sensitive: true' => ['email', 'ed@be', true, [2]],
-            'case matches, contains search string, case sensitive: false' => ['email', 'ed@be', false, [2]],
-            'case does not match, contains search string, case sensitive: null' => ['email', 'SEED@', null, [2]],
+            'search: starts with, same case, case sensitive: null' => ['email', 'seed@', null, [2]],
+            'search: ends with, same case, case sensitive: null' => ['email', '@beat', null, [2]],
+            'search: contains, same case, case sensitive: null' => ['email', 'ed@be', null, [2]],
+            'search: contains, same case, case sensitive: false' => ['email', 'ed@be', false, [2]],
+            'search: contains, same case, case sensitive: true' => ['email', 'ed@be', true, [2]],
+            'search: contains, different case, case sensitive: null' => ['email', 'SEED@', null, [2]],
+            'search: contains, different case, case sensitive: false' => ['email', 'SEED@', false, [2]],
+            'search: contains, different case, case sensitive: true' => ['email', 'SEED@', true, []],
             'wildcard is not supported' => ['email', '%st', null, []],
         ];
     }
 
     #[DataProvider('dataWithReader')]
-    public function testWithReader(string $field, string $value, bool|null $caseSensitive, array $expectedFixtureIndexes): void
+    public function testWithReader(
+        string $field,
+        string $value,
+        bool|null $caseSensitive,
+        array $expectedFixtureIndexes,
+    ): void
     {
         $reader = $this->getReader()->withFilter(new Like($field, $value, $caseSensitive));
         $this->assertFixtures($expectedFixtureIndexes, $reader->read());
