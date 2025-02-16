06a0a1b736f97d092eee591d2baadc8b945aac3e
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-02-16 14:57:24 +0300

    fix

--- src/Reader/DataReaderException.php
+++ src/Reader/DataReaderException.php
@@ -10,6 +10,8 @@ use RuntimeException;
  * Exception occurred during reading data.
  *
  * @see DataReaderInterface
+ *
+ * @psalm-suppress ClassMustBeFinal We assume that the class may be extended in userland.
  */
 class DataReaderException extends RuntimeException
 {
--- src/Writer/DataWriterException.php
+++ src/Writer/DataWriterException.php
@@ -10,6 +10,8 @@ use RuntimeException;
  * Exception occurred during writing or deleting data.
  *
  * @see DataWriterInterface
+ *
+ * @psalm-suppress ClassMustBeFinal We assume that the class may be extended in userland.
  */
 class DataWriterException extends RuntimeException
 {
