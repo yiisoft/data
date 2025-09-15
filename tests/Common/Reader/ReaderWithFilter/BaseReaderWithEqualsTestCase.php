5a6ba214ae65b79db0089cf5100bbfe1690731a9
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-09-15 22:11:38 +0300

    tests

--- tests/Common/Reader/ReaderWithFilter/BaseReaderWithEqualsTestCase.php
+++ tests/Common/Reader/ReaderWithFilter/BaseReaderWithEqualsTestCase.php
@@ -8,6 +8,7 @@ use DateTimeImmutable;
 use PHPUnit\Framework\Attributes\DataProvider;
 use Yiisoft\Data\Reader\Filter\Equals;
 use Yiisoft\Data\Tests\Common\Reader\BaseReaderTestCase;
+use Yiisoft\Data\Tests\Support\StringableValue;
 
 abstract class BaseReaderWithEqualsTestCase extends BaseReaderTestCase
 {
@@ -16,6 +17,7 @@ abstract class BaseReaderWithEqualsTestCase extends BaseReaderTestCase
         yield 'integer' => [new Equals('number', 2), [2]];
         yield 'float' => [new Equals('balance', 10.25), [1]];
         yield 'string' => [new Equals('email', 'the@best'), [4]];
+        yield 'stringable' => [new Equals('email', new StringableValue('the@best')), [4]];
         yield 'datetime' => [new Equals('born_at', new DateTimeImmutable('1990-01-01')), [5]];
     }
 
--- tests/Common/Reader/ReaderWithFilter/BaseReaderWithInTestCase.php
+++ tests/Common/Reader/ReaderWithFilter/BaseReaderWithInTestCase.php
@@ -4,14 +4,26 @@ declare(strict_types=1);
 
 namespace Yiisoft\Data\Tests\Common\Reader\ReaderWithFilter;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use Yiisoft\Data\Reader\Filter\In;
 use Yiisoft\Data\Tests\Common\Reader\BaseReaderTestCase;
+use Yiisoft\Data\Tests\Support\StringableValue;
 
 abstract class BaseReaderWithInTestCase extends BaseReaderTestCase
 {
-    public function testWithReader(): void
+    public static function dataWithReader(): iterable
     {
-        $reader = $this->getReader()->withFilter(new In('number', [2, 3]));
-        $this->assertFixtures([1, 2], $reader->read());
+        yield 'int' => [new In('number', [2, 3]), [1, 2]];
+        yield 'stringable' => [
+            new In('email', [new StringableValue('seed@beat'), new StringableValue('the@best')]),
+            [2, 3],
+        ];
+    }
+
+    #[DataProvider('dataWithReader')]
+    public function testWithReader(In $filter, array $expected): void
+    {
+        $reader = $this->getReader()->withFilter($filter);
+        $this->assertFixtures($expected, $reader->read());
     }
 }
